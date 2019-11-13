ee9681873744639d2a122829587016613a173775
Mikhail Grachev <work@mgrachev.com>
2019-11-13 21:15:01 +0300

    Add brakeman
    Signed-off-by: Mikhail Grachev <work@mgrachev.com>

--- fmts/doc.go
+++ fmts/doc.go
@@ -23,6 +23,8 @@
 // 	python
 // 		pep8	Python style guide checker - https://pypi.python.org/pypi/pep8
 // 	ruby
+// 		brakeman	(brakeman --quiet --format tabs) A static analysis security vulnerability scanner for Ruby on Rails applications - https://github.com/presidentbeef/brakeman
+// 		reek	(reek --single-line) Code smell detector for Ruby - https://github.com/troessner/reek
 // 		rubocop	A Ruby static code analyzer, based on the community Ruby style guide - https://github.com/rubocop-hq/rubocop
 // 	scala
 // 		sbt	the interactive build tool - http://www.scala-sbt.org/
